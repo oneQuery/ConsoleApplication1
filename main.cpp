bfd94116f58a954d83c3b2330e45efcab5f4d15e
ByteBrewer <heesu-kim@snuailab.ai>
2023-05-21 19:05:16 +0900

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+
+using namespace Windows::AI::MachineLearning;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Graphics::Imaging;
+using namespace Windows::Media;
+using namespace Windows::Storage;
+
+using namespace std;
+
+// Global variables
+hstring modelPath = L"C:\\Users\\heesu\\workspace\\Windows-Machine-Learning\\SharedContent\\models\\SqueezeNet.onnx";
+string deviceName = "default";
+hstring imagePath;
+LearningModel model = nullptr;
+LearningModelDeviceKind deviceKind = LearningModelDeviceKind::Default;
+LearningModelSession session = nullptr;
+LearningModelBinding binding = nullptr;
+VideoFrame imageFrame = nullptr;
+string labelsFilePath;
+vector<string> labels;
+
+// Forward declarations
+void LoadModel();
+VideoFrame LoadImageFile(hstring filePath);
+void BindModel();
+void EvaluateModel();
+void PrintResults(IVectorView<float> results);
+void LoadLabels();
+
+int main()
+{
+    init_apartment();
+    LoadModel();
+}
+
+void LoadModel()
+{
+    // load the model
+    printf("Loading modelfile '%ws' on the '%s' device\n", modelPath.c_str(), deviceName.c_str());
+    DWORD ticks = GetTickCount();
+    model = LearningModel::LoadFromFilePath(modelPath);
+    ticks = GetTickCount() - ticks;
+    printf("model file loaded in %d ticks\n", ticks);
+}
+
