e469855d6d0a56f2a07350d525709c3fff20657d
ByteBrewer <heesu-kim@snuailab.ai>
2023-05-21 21:09:56 +0900

    add: basic methods

--- main.cpp
+++ main.cpp
@@ -14,14 +14,15 @@ using namespace std;
 // Global variables
 hstring modelPath = L"C:\\Users\\heesu\\workspace\\Windows-Machine-Learning\\SharedContent\\models\\SqueezeNet.onnx";
 string deviceName = "default";
-hstring imagePath;
+hstring imagePath = L"C:\\Users\\heesu\\workspace\\Windows-Machine-Learning\\SharedContent\\media\\fish.png";
 LearningModel model = nullptr;
 LearningModelDeviceKind deviceKind = LearningModelDeviceKind::Default;
 LearningModelSession session = nullptr;
 LearningModelBinding binding = nullptr;
 VideoFrame imageFrame = nullptr;
-string labelsFilePath;
+string labelsFilePath = "C:\\Users\\heesu\\workspace\\Windows-Machine-Learning\\Samples\\SqueezeNetObjectDetection\\Desktop\\cpp\\Labels.txt";
 vector<string> labels;
+
 
 // Forward declarations
 void LoadModel();
@@ -35,6 +36,9 @@ int main()
 {
     init_apartment();
     LoadModel();
+    imageFrame = LoadImageFile(imagePath);
+    BindModel();
+    EvaluateModel();
 }
 
 void LoadModel()
@@ -47,3 +51,120 @@ void LoadModel()
     printf("model file loaded in %d ticks\n", ticks);
 }
 
+VideoFrame LoadImageFile(hstring filePath)
+{
+    printf("Loading the image...\n");
+    DWORD ticks = GetTickCount();
+    VideoFrame inputImage = nullptr;
+    
+
+    try
+    {
+        // open the file
+        StorageFile file = StorageFile::GetFileFromPathAsync(filePath).get();
+        // get a stream on it
+        auto stream = file.OpenAsync(FileAccessMode::Read).get();
+        // Create the decoder from the stream
+        BitmapDecoder decoder = BitmapDecoder::CreateAsync(stream).get();
+        // get the bitmap
+        SoftwareBitmap softwareBitmap = decoder.GetSoftwareBitmapAsync().get();
+        // load a videoframe from it
+        inputImage = VideoFrame::CreateWithSoftwareBitmap(softwareBitmap);
+    }
+    catch (...)
+    {
+        printf("failed to load the image file, make sure you are using fully qualified paths\r\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ticks = GetTickCount() - ticks;
+    printf("image file loaded in %d ticks\n", ticks);
+    // all done
+    return inputImage;
+}
+
+void BindModel()
+{
+    printf("Binding the model...\n");
+    DWORD ticks = GetTickCount();
+
+    // now create a session and binding
+    session = LearningModelSession{ model, LearningModelDevice(deviceKind) };
+    binding = LearningModelBinding{ session };
+    // bind the intput image
+    binding.Bind(L"data_0", ImageFeatureValue::CreateFromVideoFrame(imageFrame));
+    // bind the output
+    vector<int64_t> shape({ 1, 1000, 1, 1 });
+    binding.Bind(L"softmaxout_1", TensorFloat::Create(shape));
+
+    ticks = GetTickCount() - ticks;
+    printf("Model bound in %d ticks\n", ticks);
+}
+
+void EvaluateModel()
+{
+    // now run the model
+    printf("Running the model...\n");
+    DWORD ticks = GetTickCount();
+
+    auto results = session.Evaluate(binding, L"RunId");
+
+    ticks = GetTickCount() - ticks;
+    printf("model run took %d ticks\n", ticks);
+
+    // get the output
+    auto resultTensor = results.Outputs().Lookup(L"softmaxout_1").as<TensorFloat>();
+    auto resultVector = resultTensor.GetAsVectorView();
+    PrintResults(resultVector);
+}
+
+void PrintResults(IVectorView<float> results)
+{
+    // load the labels
+    LoadLabels();
+    // Find the top 3 probabilities
+    vector<float> topProbabilities(3);
+    vector<int> topProbabilityLabelIndexes(3);
+    // SqueezeNet returns a list of 1000 options, with probabilities for each, loop through all
+    for (uint32_t i = 0; i < results.Size(); i++)
+    {
+        // is it one of the top 3?
+        for (int j = 0; j < 3; j++)
+        {
+            if (results.GetAt(i) > topProbabilities[j])
+            {
+                topProbabilityLabelIndexes[j] = i;
+                topProbabilities[j] = results.GetAt(i);
+                break;
+            }
+        }
+    }
+    // Display the result
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%s with confidence of %f\n", labels[topProbabilityLabelIndexes[i]].c_str(), topProbabilities[i]);
+    }
+}
+
+void LoadLabels()
+{
+    // Parse labels from labels file.  We know the file's entries are already sorted in order.
+    ifstream labelFile{ labelsFilePath, ifstream::in };
+    if (labelFile.fail())
+    {
+        printf("failed to load the %s file.  Make sure it exists in the same folder as the app\r\n", labelsFilePath.c_str());
+        exit(EXIT_FAILURE);
+    }
+
+    std::string s;
+    while (std::getline(labelFile, s, ','))
+    {
+        int labelValue = atoi(s.c_str());
+        if (labelValue >= labels.size())
+        {
+            labels.resize(labelValue + 1);
+        }
+        std::getline(labelFile, s);
+        labels[labelValue] = s;
+    }
+}
